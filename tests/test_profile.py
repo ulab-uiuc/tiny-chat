e9d43f3a938ea2fa7fdfb098b092a5f2261f22f1
Yining Zhao <zhaoyining5445@gmail.com>
2025-08-14 09:15:17 +0800

    update

--- tests/test_profile.py
+++ tests/test_profile.py
@@ -3,22 +3,22 @@ from tiny_chat.profiles import BaseAgentProfile, BaseEnvironmentProfile
 
 def test_agent_profile():
     profile = BaseAgentProfile(
-        first_name="John",
-        last_name="Doe",
+        first_name='John',
+        last_name='Doe',
         age=25,
-        occupation="Software Engineer",
+        occupation='Software Engineer',
     )
 
-    profile.add_field("mbti", "intj")
-    profile.remove_field("occupation")
+    profile.add_field('mbti', 'intj')
+    profile.remove_field('occupation')
     xml_profile = profile.to_background_string(agent_id=1)
     print(xml_profile)
 
 
 def test_environment_profile():
-    profile = BaseEnvironmentProfile(scenario="Three agents are eating dinner")
+    profile = BaseEnvironmentProfile(scenario='Three agents are eating dinner')
     print(profile)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     test_environment_profile()
